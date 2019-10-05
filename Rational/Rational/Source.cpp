6e53f6c2c20585d13c507eb801f9690ac66d6ba8
BublIk2005 <55318265+BublIk2005@users.noreply.github.com>
2019-10-05 16:45:11 +0300

    Много изменений, теперь все работает

--- Rational/Rational/Source.cpp
+++ Rational/Rational/Source.cpp
@@ -1,148 +1,153 @@
-#include<iostream>
+#include <iostream>
+#include <sstream>
+
 
 using namespace std;
 
-struct Rational
-{
-	int m{ 0 };
-	int n{ 1 };
-	Rational();
-	int NOD(int n1, int n2)
-	{
-		int div;
-		if (n1 == n2)  return n1;
-		int d = n1 - n2;
-		if (d < 0) {
-			d = -d;  div = NOD(n1, d);
-		}
-		else
-			div = NOD(n2, d);
-		return div;
-	}
-	int NOK(int n1, int n2)
-	{
-		return n1 * n2 / NOD(n1, n2);
-	}
-	
-	explicit Rational(const int a)
-	{
-		m = a;
-	}
-	Rational(const int a,const int b)
-	{
-		m = a/ NOD(a, b);
-		n = b/ NOD(a, b);
-		if (n < 0)
-		{
-			n *= -1;
+int NOD(int n1, int n2);
+int NOK(int n1, int n2);
+
+struct Rational {
+	int n{ 0 };
+	int m{ 1 };
+
+	Rational() {};
+
+	explicit Rational(int m1) {
+		m = m1;
+	};
+	Rational(const int m1, const int n1) {
+		m = m1 / NOD(n1, m1);
+		n = n1 / NOD(n1, m1);
+		if (n < 0) {
 			m *= -1;
+			n *= -1;
 		}
+	};
+
+	Rational operator-(const Rational& rhs);
+	Rational& operator+=(const Rational& rhs);
+	Rational& operator-=(const Rational& rhs);
+	Rational& operator*=(const Rational& rhs);
+	Rational& operator/=(const Rational& rhs);
+	bool operator==(const Rational& rhs) {
+		return (m == rhs.m) && (n == rhs.n);
+	}
+	bool operator!=(const Rational& rhs) {
+		return !operator==(rhs);
+	}
+	bool operator>(const Rational& rhs) {
+		return ((*this - rhs).m > 0);
+	}
+	bool operator>=(const Rational& rhs) {
+		return (*this - rhs).m >= 0;
+	}
+	bool operator<(const Rational& rhs) {
+		return !operator>=(rhs);
+	}
+	bool operator<=(const Rational& rhs) {
+		return !operator>(rhs);
 	}
 
-	bool operator==(const Rational& rat)
-	{
-		return ((Rational(m, n) - rat)==Rational(0));
-	}
-	
-	bool operator!=(const Rational& rat)
-	{
-		return (Rational(m, n) != rat);
-	}
-	
-	Rational& operator +=(const Rational& r)
-	{
-		int k = NOK(n, r.n);
-		m = (m * k / n) + (r.m * k / r.n);
-		n = k;
-		return *this;
-	}
-	Rational operator +(const Rational& r)
-	{
-		int k = NOK(n, r.n);
-		return Rational((m * k / n) + (r.m * k / r.n), k);
-	}
-	Rational& operator -=(const Rational& r)
-	{
-		int k = NOK(n, r.n);
-		m = (m * k / n) - (r.m * k / r.n);
-		n = k;
-		return *this;
-	}
-	Rational operator -(const Rational& r)
-	{
-		int k = NOK(n, r.n);
-		return Rational((m * k / n) - (r.m * k / r.n), k);
-	}
-	Rational operator *(const Rational r)
-	{
-		return Rational(m*r.m, n*r.n);
-	}
-	
-	Rational operator /(const Rational r)
-	{
-		return Rational(m*r.n, n*r.m);
-	}
-	Rational operator /=(const Rational r)
-	{
-		m = m * r.n;
-		n = n * r.m;
-		int k = NOD(m, n);
-		m /= k;
-		n /= k;
-		return *this;
-	}
-	Rational operator *=(const Rational r)
-	{
-		m = m * r.n;
-		n = n * r.m;
-		int k = NOD(m, n);
-		m /= k;
-		n /= k;
-		return *this;
-	}
-	bool operator>(const Rational r)
-	{
-		return((Rational(m, n) - r).n > 0);
-	}
-	bool operator<(const Rational r)
-	{
-		return((Rational(m, n) - r).n < 0);
-	}
-	bool operator>=(const Rational r)
-	{
-		return((Rational(m, n) - r).n >= 0);
-	}
-	bool operator<=(const Rational r)
-	{
-		return((Rational(m, n) - r).n >= 0);
-	}
-	ostream& WriteFrom(ostream ostrm)
-	{
-		ostrm << n << '/' << m;
-		return ostrm;
-	}
-	istream& ReadFrom(istream istrm)
-	{
-		char separator{ '/' };
-		istrm >> m >> separator >> n;
-	}
-	ostream& writeTo(std::ostream& ostrm) const
-	{
+	ostream& writeTo(ostream& ostrm) const;
+	istream& readFrom(istream& istrm);
 
-	};
-	istream& readFrom(std::istream& istrm);
-	};
-	inline std::ostream& operator<<(std::ostream& ostrm, const Rational& rhs)
-		 {
-		 return rhs.writeTo(ostrm);
-		 }
-	
-		 inline std::istream& operator>>(std::istream& istrm, Rational& rhs)
-		 {
-		 return rhs.readFrom(istrm);
-		 }
-int main()
+};
+
+Rational operator+(const Rational& lhs, const Rational& rhs);
+Rational operator*(const Rational& lhs, const Rational& rhs);
+Rational operator/(const Rational& lhs, const Rational& rhs);
+
+inline ostream& operator<<(ostream& ostrm, const Rational& rhs) {
+	return rhs.writeTo(ostrm);
+}
+inline istream& operator>>(istream& istrm, Rational& rhs) {
+	return rhs.readFrom(istrm);
+}
+
+int main() {
+	Rational a (1,1);
+	Rational b;
+	cout << a;
+	system("pause");
+}
+
+int NOD(int n1, int n2)
 {
-	Rational r(1,2);
-	cout << r;
+	n1 = abs(n1);
+	n2 = abs(n2);
+	int div;
+	if (n1 == n2)  return n1;
+	int d = n1 - n2;
+	if (d < 0) {
+		d = -d;  div = NOD(n1, d);
+	}
+	else
+		div = NOD(n2, d);
+	return div;
+}
+int NOK(int n1, int n2)
+{
+	n1 = abs(n1);
+	n2 = abs(n2);
+	return n1 * n2 / NOD(n1, n2);
+}
+Rational Rational::operator-(const Rational& rhs) {
+	int k = NOK(n, rhs.n);
+	return Rational((m * k / n) - (rhs.m * k / rhs.n), k);
+}
+Rational& Rational::operator+=(const Rational& rhs) {
+	int k = NOK(n, rhs.n);
+	m = n / k + rhs.n / k;
+	n = k;
+	return *this;
+}
+Rational& Rational::operator-=(const Rational& rhs) {
+	int k = NOK(n, rhs.n);
+	m = n / k - rhs.n / k;
+	n = k;
+	return *this;
+}
+Rational& Rational::operator*=(const Rational& rhs) {
+	m *= rhs.m;
+	n *= rhs.n;
+	int k = NOD(m, n);
+	m /= k;
+	n /= k;
+	return *this;
+}
+Rational& Rational::operator/=(const Rational& rhs) {
+	m *= rhs.n;
+	n *= rhs.m;
+	int k = NOD(m, n);
+	m /= k;
+	n /= k;
+	if (n < 0) {
+		n *= -1;
+		m *= -1;
+	}
+	return *this;
+}
+Rational operator+(const Rational& lhs, const Rational& rhs) {
+	int k = NOK(lhs.n, rhs.n);
+	return Rational((lhs.m * k / lhs.n) + (rhs.m * k / rhs.n), k);
+}
+Rational operator*(const Rational& lhs, const Rational& rhs) {
+	return Rational(lhs.m * rhs.m, lhs.n * rhs.n);
+}
+Rational operator/(const Rational& lhs, const Rational& rhs) {
+	return Rational(lhs.m / rhs.n, lhs.n * rhs.m);
+}
+ostream& Rational::writeTo(ostream& ostrm) const {
+	ostrm << m << "/" << n;
+	return ostrm;
+}
+istream& Rational::readFrom(istream& istrm) {
+	char sep;
+	int m1, n1;
+	istrm >> m1 >> sep >> n1;
+	Rational temp(m1, n1);
+	m = temp.m;
+	n = temp.n;
+	return istrm;
 }
